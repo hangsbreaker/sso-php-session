9abad4941f440ef2f5c77da3040798af1811d8f6
Hangs Breaker <hangsbreaker@gmail.com>
2016-11-28 11:06:45 +0700

    Update sso_manager.php

--- manager/sso_manager.php
+++ manager/sso_manager.php
@@ -4,6 +4,13 @@ $this_page = "http://".$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
 if(isset($_GET['s'])){
 	header("location:".$GLOBALS['this_page']);
 }
+// if logout
+if(isset($_GET['o'])){
+	if(isset($_GET['u'])){
+		$_SESSION['url'] = array_diff($_SESSION['url'],array($_GET['u']));
+	}
+	$this->sso_logout();
+}
 //check is login redirect to url login
 function is_login($url=''){
 	if(isset($_GET['u'])){
@@ -11,44 +18,45 @@ function is_login($url=''){
 			sso_login($_GET['u']);
 		}
 	}else if(!empty($_SESSION)){
-		if(!empty($url)){
+		if(!empty($url) && !isset($_GET['o'])){
 			header("location:".$url);
-		}else if(!empty($_SESSION['url'])){
-			if(count($_SESSION['url']) > 0){
+		}else{
+			if(!empty($_SESSION['url'])){
 				$url=end($_SESSION['url']);
 				if($url!='' && $url!=$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME']){
-					sso_login($url);
-				}
+				sso_login($url);
 			}
 		}
 	}
 }
 // Adding session url from get
 function sso_login($url=''){
-	$url = isset($_GET['u'])?$_GET['u']:$url;
-	$host_login = "http://".$_SERVER['HTTP_HOST'];
-	$host_web = "http://".parse_url($url,PHP_URL_HOST);
-	if(isset($_SESSION['url'])){
-		if(!in_array($url,$_SESSION['url'])){
-			if($host_login != $host_web){
-				array_push($_SESSION['url'],$url);
-			}else if($host_login == $host_web){
-				if(!in_array($host_login,$_SESSION['url'])){
-					array_push($_SESSION['url'],$GLOBALS['this_page']);
+	if(!isset($_GET['o'])){
+		$url = isset($_GET['u'])?$_GET['u']:$url;
+		$host_login = "http://".$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME'];
+		$host_web = "http://".parse_url($url,PHP_URL_HOST);
+		if(isset($_SESSION['url'])){
+			if(!in_array($url,$_SESSION['url'])){
+				if($host_login != $host_web){
+					array_push($_SESSION['url'],$url);
+				}else if($host_login == $host_web){
+					if(!in_array($host_login,$_SESSION['url'])){
+						array_push($_SESSION['url'],"http://".$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME']);
+					}
 				}
 			}
-		}
-	}else{
-		if($host_login == $host_web){
-			$_SESSION['url']=array($GLOBALS['this_page']);
 		}else{
-			$_SESSION['url']=array($url);
+			if($host_login == $host_web){
+				array_push($_SESSION['url'],"http://".$_SERVER['HTTP_HOST'].$_SERVER['SCRIPT_NAME']);
+			}else{
+				$_SESSION['url']=array($url);
+			}
 		}
+		$_SESSION['url'] = array_diff($_SESSION['url'],array(''));
+		$json = json_encode($_SESSION);
+		// send session to other page with encrypted using base64 encode
+		header("location:".$url."?s=".base64_encode($json));
 	}
-	$_SESSION['url'] = array_diff($_SESSION['url'],array(''));
-	$json = json_encode($_SESSION);
-	// send session to other page with encrypted using base64 encode
-	header("location:".$url."?s=".base64_encode($json));
 }
 
 // Logout system base on url session
@@ -56,14 +64,14 @@ function sso_logout(){
 	if(!empty($_SESSION['url'])){
 		if(count($_SESSION['url']) > 0){
 			$url=end($_SESSION['url']);
-			$_SESSION['url'] = array_diff($_SESSION['url'],array($url));
+			//$_SESSION['url'] = array_diff($_SESSION['url'],array($url));
 			header("location:".$url."?o=l&u=".$GLOBALS['this_page']);
 		}
 	}else{
 		session_destroy();
 		header("location:".$GLOBALS['this_page']);
 	}
-	echo "Harap tunggu...";
+	echo "Please Wait...";
 }
 //print_r($_SESSION);
 ?>
